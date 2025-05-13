3948156440beb335094f7e31cc53cd9d3fab941b
Khisan010 <skorpionsheyh@gmail.com>
2025-05-13 22:51:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let malumot1=prompt("string typedagi malumot kiriting")
+let malumot2=+prompt("number typedagi malumot kiriting")
+let malumot3=prompt("boolean typedagi malumot kiriting")
+let confir=confirm("hamma malumotlarni togri kiritganizga ishonchingiz komilmi?")
+alert(`siz kiritgan ${malumot1} string typega tegishli
+    siz kiritgan ${malumot2} number typega tegishli
+    siz kiritgan ${malumot3} boolean typega tegishli`)
